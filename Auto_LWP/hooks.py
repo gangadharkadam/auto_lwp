2d87eb61f8b3dda7240b3cf1e95c516054cc5e2e
Gangadhar Kadam <gangadhar.k@indictanstech.com>
2015-11-10 18:18:31 +0530

    first commit

---
+++ Auto_LWP/hooks.py
@@ -0,0 +1,106 @@
+app_name = "Auto_LWP"
+app_title = "Auto LWP"
+app_publisher = "Frappe"
+app_description = "Automatic lwp calculation"
+app_icon = "icon-table"
+app_color = "#589494"
+app_email = "lwp@gmail.com"
+app_version = "0.0.1"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/Auto_LWP/css/Auto_LWP.css"
+# app_include_js = "/assets/Auto_LWP/js/Auto_LWP.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/Auto_LWP/css/Auto_LWP.css"
+# web_include_js = "/assets/Auto_LWP/js/Auto_LWP.js"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+
+# before_install = "Auto_LWP.install.before_install"
+# after_install = "Auto_LWP.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "Auto_LWP.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"Auto_LWP.tasks.all"
+# 	],
+# 	"daily": [
+# 		"Auto_LWP.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"Auto_LWP.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"Auto_LWP.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"Auto_LWP.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "Auto_LWP.install.before_tests"
+
+# Overriding Whitelisted Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "Auto_LWP.event.get_events"
+# }
+
